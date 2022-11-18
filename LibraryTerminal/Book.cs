8c097cb92b085121503558db74662586cdee1f28
ktMcD <geek.katie@gmail.com>
2022-11-17 19:59:45 -0800

    Updates to Book
    Changed the property "BookName" to "Title". Updated the order in which the parameters are sent to Book's constructor. Changed the return book method to use the property variable name rather than the enum class name for assigning the book status.

--- LibraryTerminal/Book.cs
+++ LibraryTerminal/Book.cs
@@ -4,14 +4,14 @@ namespace LIBRARY_TERMINAL
     public class Book
     {
         public DateTime today = DateTime.Today;
-        public string BookName { get; set; }
+        public string Title { get; set; }
         public string Author { get; set; }
         public BookStatus Status { get; set; }
         public  DateTime DueDate { get; set; } = new DateTime();
-        public Book(string author, string bookName)
+        public Book(string bookTitle, string bookAuthor)
         {
-            BookName = bookName;
-            Author = author;
+            Title = bookTitle;
+            Author = bookAuthor;
             Status = 0;
 
         }
@@ -25,7 +25,7 @@ namespace LIBRARY_TERMINAL
 
         public List<Book> SearchBookByTitle(List<Book> book, string title)
         {
-            var searchBookByTitle = book.Where(x => x.BookName.Contains(title, StringComparison.InvariantCultureIgnoreCase)).Select(x => x).ToList();
+            var searchBookByTitle = book.Where(x => x.Title.Contains(title, StringComparison.InvariantCultureIgnoreCase)).Select(x => x).ToList();
 
 
             return searchBookByTitle;
@@ -33,18 +33,16 @@ namespace LIBRARY_TERMINAL
         }
         public void CheckOutBook(List<Book> book, string bookTitle)
         {
-            
             this.Status = (BookStatus)1;
             this.DueDate = today.AddDays(14);
-
         }
 		
 		public void ReturnABook(string bookTitle)
         {
-            
-            BookStatus = 0;
+            // katie updated this to get it to compile. We use the variable name for BookStatus here. :)
+            // should we use the same pattern that Shan used in CheckOutBook?
+            this.Status = (BookStatus)0; 
             DueDate = DateTime.Today;
-             
         }
     }
 }
